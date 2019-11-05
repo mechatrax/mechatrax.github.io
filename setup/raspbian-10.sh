ef5e74e5e1911500bf7c8e31322c035cebd23e19
Masahiro Honda <honda@mechatrax.com>
2019-11-05 11:42:25 +0900

    Update setup scripts

--- setup/raspbian-10.sh
+++ setup/raspbian-10.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-TMPDIR=$(mktemp -d /tmp/io.github.mechatrax.raspbian.stretch.setup.XXXXXXX)
 REPO=http://mechatrax.github.io/raspbian/
 DIST=buster
 DEB=mechatrax-archive-keyring_2016.12.19.2_all.deb
 SHA256=3149c46ec82f3b1763537e4c6029923f849f4cd00a08e9b02255b05f4240fcfb
+TMPDIR=$(mktemp -d /tmp/io.github.mechatrax.raspbian.${DIST}.setup.XXXXXXX)
 
 pushd $TMPDIR
 echo "$SHA256 $DEB" > sha256.txt
